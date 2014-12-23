d89e4dbe1b56946c8d5aa03f36230dda8db429e5
Minuri-Rajapaksha <rpmcrajapaksha@gmail.com>
2014-12-23 06:51:41 +0530

    Presentations

--- codings/backups/pawani akka - Copy/codings/jpl-1.0.1/jpl-1.0.1/jsrc/jpl/Version.java
+++
@@ -1,9 +0,0 @@
-// $Id: Version.java,v 1.14 1999/05/05 20:29:54 fadushin Exp $
-package jpl;
-class Version
-{
-	public final java.lang.String status = "final";
-	public final int    major            = 1;
-	public final int    minor            = 0;
-	public final int    patch            = 1;
-}
--- codings/backups/pawani akka/codings/jpl-1.0.1/jpl-1.0.1/demo/getting-started/test.pl
+++
@@ -1,9 +0,0 @@
-child_of( joe, ralf ).
-child_of( mary, joe ).
-child_of( steve, joe ).
-
-descendent_of( X, Y ) :-
-	child_of( X, Y ).
-descendent_of( X, Y ) :-
-	child_of( Z, Y ),
-	descendent_of( X, Z ).
